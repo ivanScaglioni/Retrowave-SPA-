936955d7af651dfaed8b7e899fba0bbb85023b12
ivanScaglioni <ivanscargentino@gmail.com>
2022-03-21 20:05:47 -0300

    error import img typescript

--- frontend/src/components/Skills.vue
+++ frontend/src/components/Skills.vue
@@ -1,156 +1,220 @@
 <template>
-    <section id="skills">
-        <div class="tabs">
-            <button @click="see('skill')">all</button>
-            <button @click="see('frontend')" >frontend</button>
-            <button @click="see('backend')">backend</button>
-            <button @click="see('others')">others</button>
-        </div>
-        <div id="skills-container">
-            <div class="skill frontend" >
-                <p class="name-skill">Javascript  </p>
-                <img class="logo-skill" src="../../public/img/logos/js-logo.png" >
-            </div>
-            <div class="skill frontend">
-                <p class="name-skill">Typescript</p>
-                <img class="logo-skill" src="../../public/img/logos/ts-logo.png" >
-            </div>
-            <div class="skill frontend">
-                <p class="name-skill">Vue</p>
-                <img class="logo-skill" src="../../public/img/logos/vue-logo.png" >
-            </div>
-            <div class="skill backend">
-                <p class="name-skill">Nodejs</p>
-                <img class="logo-skill" src="../../public/img/logos/node-logo.png" >
-            </div>
-            <div class="skill others">
-                <p class="name-skill">Git</p>
-                <img class="logo-skill" src="../../public/img/logos/git-logo.png" >
-            </div>
-            <div class="skill backend">
-                <p class="name-skill">MongoDB</p>
-                <img class="logo-skill" src="../../public/img/logos/mongo-logo.png" >
-            </div>
-            <div class="skill frontend">
-                <p class="name-skill">Webpack</p>
-                <img class="logo-skill" src="../../public/img/logos/wp-logo.png" >
-            </div>
-            <div class="skill frontend">
-                <p class="name-skill">CSS</p>
-                <img class="logo-skill" src="../../public/img/logos/css-logo.png" >
-            </div>
-            <div class="skill others">
-                <p class="name-skill">Postman</p>
-                <img class="logo-skill" src="../../public/img/logos/pm-logo.png" >
-            </div>
-            <div class="skill frontend">
-                <p class="name-skill">Boostrap</p>
-                <img class="logo-skill" src="../../public/img/logos/bt-logo.png" >
-            </div>
-            <div class="skill frontend">
-                <p class="name-skill">HTML</p>
-                <img class="logo-skill" src="../../public/img/logos/html-logo.png" >
-            </div>
-        </div>
+  <section id="skills">
+    <!-- <div class="title">Skills</div> -->
+    <div class="tabs section ">
+      <button id="btn-skill" @click="see('skill')">all</button>
+      <button id="btn-frontend" @click="see('frontend')">frontend</button>
+      <button id="btn-backend" @click="see('backend')">backend</button>
+    </div>
 
-    </section>
+    <div id="skills-container">
+      <div id="conte">
 
+      </div>
+    </div>
+  </section>
 </template>
 
 <script lang="ts">
-import {defineComponent} from 'vue';
+"use strict"
+
+
+import * as icon from '../../public/icons/icon-vue.png';
+
+import {  defineComponent } from "vue";
+
 
 
 
 
 
 export default defineComponent({
-    data(){
-        return{
-        }
-    },
-    methods:{
-        async see(seeSkill:string){
-            const skills = await document.getElementsByClassName('skill') as HTMLCollectionOf<HTMLElement>;
-            const arr = Array.from(skills, skill => skill.className );
-            for (let index = 0; index < arr.length; index++) {
-                if(arr[index].includes(seeSkill)){
-                    skills[index].style.display="block";
-                }else{
-                    skills[index].style.display = "none";
-                }
-            }
-        }
-        
-    },
+  data() {
+    return {
+      currentTap: "skill",
 
+      rute: 'public/icons/',
+
+      arrSkills: [
+        {
+          name:'Javascript',
+          nameIcon:'icon-gmail.svg' 
+        },
+        {
+          name: 'Typescript',
+          nameIcon: 'icon-typescript.svg'
+        },
+        {
+          name:'MongoDB',
+          nameIcon:'icon-mongodb.svg'
+        },
+        {
+          name:'Node.js',
+          nameIcon:'icon-node.svg'
+        },
+        {
+          name:'Vue',
+          nameIcon:'icon-vue.png'
+        },
+        {
+          name:'HTML 5',
+          nameIcon: 'icon-html5.svg'
+        },
+        {
+          name:'CSS',
+          nameIcon:'icon-css.svg'
+        },
+        {
+          name:'Webpack',
+          nameIcon:'icon.webpack.svg'
+        },
+        {
+          name:'Git',
+          nameIcon:'icon-git.svg'
+        }
         
+      ]
+
+      
+      
+    }
+  },
+
+  methods: {
+    async see(seeSkill: string) {
+      const skills = (await document.getElementsByClassName(
+        "skill"
+      )) as HTMLCollectionOf<HTMLElement>;
+      const tap = (await document.getElementById(
+        `btn-${this.currentTap}`
+      )) as HTMLElement;
+      const btn = (await document.getElementById(
+        `btn-${seeSkill}`
+      )) as HTMLElement;
+      tap.style.backgroundColor = "rgba(42,71,94,0)";
+      btn.style.backgroundColor = "rgba(42,71,94,1.0)";
+      this.currentTap = seeSkill;
+      const arr = Array.from(skills, (skill) => skill.className);
+      for (let index = 0; index < arr.length; index++) {
+        if (arr[index].includes(seeSkill)) {
+          skills[index].style.display = "block";
+        } else {
+          skills[index].style.display = "none";
+        }
+      }
+    },
+  },
+
+  mounted() {
+    const tab = document.getElementById("btn-skill") as HTMLElement;
+    tab.style.backgroundColor = "rgba(42,71,94,1.0)";
+    const contenedor= document.getElementById('conte') as HTMLElement;
+    
+    for (let index = 0; index < this.arrSkills.length; index++) {
+      const skilldiv = document.createElement("div");
+      const skillicon = document.createElement("img");
+      const skillname = document.createElement("p");
+      
+      skillicon.src = this.rute + this.arrSkills[index].nameIcon;
+      skillname.innerHTML = this.arrSkills[index].name
+      
+      skilldiv.appendChild(skillname);
+      skilldiv.appendChild(skillicon);
+
+      contenedor.appendChild(skilldiv);
+
+    }
     
 
 
 
 
-})
+  },
+  
+
+  
+
+
+});
 </script>
 
 
 <style>
-
-#skills{
-    display: flex;
-    flex-direction: column;
-    background-color: transparent;
-    overflow: auto;
-    
+#skills {
+  display: flex;
+  flex-direction: column;
+  background-color: transparent;
+  
 }
 
-
-.logo-skill{
-    width: 50px;
-    object-fit: cover;
-    filter: grayscale(100%);
-    transition-duration: 0.5s;
+.conta {
+  display: flex;
+  flex-wrap: wrap;
+  border: 1px solid black;
+  align-self: flex-start;
+  width: 90vw;
+  min-width: 400px;
+  max-width: 1000px;
+  min-height: 1000px;
+  justify-content: center;
+  
 }
 
-.logo-skill:hover{
-    filter: contrast(3);
-    filter: saturate(3);
-    filter: brightness(0.9);
-    filter: grayscale(0);
+.logo-skill {
+  width: 5vw;
+  min-width: 70px;
+  /* #1b2838 */
+
+
+  border: 1px solid #b3dfff;
+
+  /* filter: grayscale(100%); */
+  transition-duration: 0.8s;
+}
+
+/* .logo-skill:hover{
+    filter: contrast(2);
+    filter: saturate(2);
     transform: scale(1.5);
-}
-
-.ocultar{
-    display: none;
-}
-
-.skill{
-    display: flex;
-    min-width: 90px;
-    min-height: 90px;
-    align-items: center;
-    flex-direction: column;
-    border: 2px solid red;
-
-}
-
-#skills-container{
-    display: flex;
-    border: 3px solid blue;  
-    flex-wrap: wrap;
-    color: yellow;
+    opacity: 1;
+    z-index: 100;
     
-    padding: 15px;
-    gap: 30px 30px;
-    justify-content: space-around;
-    
-    
-}
-.tabs{
-    border: 3px solid white;
+} */
+
+.ocultar {
+  display: none;
 }
 
+.skill {
+  display: block;
+  padding: 20px;
+  min-width: 130px;
+  min-height: 120px;
+  flex-direction: column;
+  border: 1px solid rebeccapurple;
+  text-align: center;
+}
+
+#skills-container {
+  width: 100%;
+  display: flex;
+  min-height: 500px;
+  flex-wrap: wrap;
+  color: yellow;
+  background: linear-gradient(
+    to bottom,
+    rgba(42, 71, 94, 1) 5%,
+    rgba(42, 71, 94, 0) 70%
+  );
+  
+  justify-content: center;
+}
 
 
-
+button {
+  background-color: transparent;
+  border: none;
+  color: lightblue;
+  font-size: 30px;
+}
 </style>
